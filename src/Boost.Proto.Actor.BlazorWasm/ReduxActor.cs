cc146637826d1a0b3846e303d183d8ae19eef7a2
심승용 <seungyongshim@nexon.co.kr>
2022-01-03 13:48:48 +0900

    코드 수정

---
+++ src/Boost.Proto.Actor.BlazorWasm/ReduxActor.cs
@@ -0,0 +1,20 @@
+using Proto;
+
+namespace Boost.Proto.Actor.BlazorWasm;
+
+public class ReduxActor<TState>
+{
+    protected ReduxActor(TState state) => State = state;
+
+    private TState State { get; set; }
+
+    protected Task ChangeStateAsync(IContext c, Func<TState, TState> func)
+    {
+        State = func(State);
+        if (State != null)
+        {
+            c.System.EventStream.Publish(State);
+        }
+        return Task.CompletedTask;
+    }
+}

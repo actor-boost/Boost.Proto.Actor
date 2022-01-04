557f7b205cd70695f50ee61c7b2d54769d562ba3
SeungYongShim <syshim@me.com>
2022-01-04 23:44:43 +0900

    리펙토링

--- src/Boost.Proto.Actor.Hosting/ProtoActorHostedService.cs
+++ src/Boost.Proto.Actor.Hosting/ProtoActorHostedService.cs
@@ -5,31 +5,16 @@ namespace Boost.Proto.Actor.Hosting
 {
     public delegate void ProtoActorHostedServiceStart(IServiceProvider serviceProvider, IRootContext root);
 
-    internal class ProtoActorHostedService : IHostedService
+    internal record ProtoActorHostedService(IServiceProvider ServiceProvider,
+                                            IRootContext Root,
+                                            ProtoActorHostedServiceStart ProtoActorHostedServiceStart): IHostedService
     {
-        public ProtoActorHostedService(IServiceProvider serviceProvider,
-                                       IRootContext root,
-                                       ProtoActorHostedServiceStart protoActorHostedServiceStart)
-        {
-            ServiceProvider = serviceProvider;
-            Root = root;
-            ProtoActorHostedServiceStart = protoActorHostedServiceStart;
-        }
-
-        public IServiceProvider ServiceProvider { get; }
-        public IRootContext Root { get; }
-        public ProtoActorHostedServiceStart ProtoActorHostedServiceStart { get; }
-
         public async Task StartAsync(CancellationToken cancellationToken)
         {
             ProtoActorHostedServiceStart?.Invoke(ServiceProvider, Root);
-
             await Task.Delay(300);
         }
 
-        public async Task StopAsync(CancellationToken cancellationToken)
-        {
-            await Root.System.ShutdownAsync();
-        }
+        public async Task StopAsync(CancellationToken cancellationToken) => await Root.System.ShutdownAsync();
     }
 }

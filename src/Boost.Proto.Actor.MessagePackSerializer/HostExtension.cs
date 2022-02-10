e560d4394f2c13a37e065ca009d5a484a9c9ba7c
심승용 <seungyongshim@nexon.co.kr>
2022-02-10 18:04:52 +0900

    코드 수정

--- src/Boost.Proto.Actor.MessagePackSerializer/HostExtension.cs
+++ src/Boost.Proto.Actor.MessagePackSerializer/HostExtension.cs
@@ -4,14 +4,19 @@ namespace Boost.Proto.Actor.MessagePackSerializer
 {
     public static class HostExtension
     {
-        public static IServiceCollection AddMessagePack(this IServiceCollection services) =>
-        services.AddSingleton(MessagePack
-            .MessagePackSerializer
-            .Typeless
-            .DefaultOptions
-            .WithAllowAssemblyVersionMismatch(true)
-            //.WithCompression(MessagePackCompression.Lz4Block)
-            .WithOmitAssemblyVersion(true)
-        );
+        public static IServiceCollection AddMessagePack(this IServiceCollection services)
+        {
+            services.AddSingleton(MessagePack
+                .MessagePackSerializer
+                .Typeless
+                .DefaultOptions
+                .WithAllowAssemblyVersionMismatch(true)
+                //.WithCompression(MessagePackCompression.Lz4Block)
+                .WithOmitAssemblyVersion(true));
+
+            services.AddSingleton(sp => ActivatorUtilities.CreateInstance<MessagePackSerializer>(sp));
+
+            return services;
+        }
     }
 }

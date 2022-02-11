aa6bae21fbd760382f6118041cb235476b95b3b7
심승용 <seungyongshim@nexon.co.kr>
2022-02-11 10:27:02 +0900

    코드 수정

--- src/Boost.Proto.Actor.Opentelemetry/OpenTelemetryDecorators.cs
+++ src/Boost.Proto.Actor.Opentelemetry/OpenTelemetryDecorators.cs
@@ -4,6 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Boost.Proto.Actor.Opentelemetry;
+using LanguageExt;
 using OpenTelemetry.Trace;
 
 using Proto.Mailbox;
--- src/Boost.Proto.Actor.Opentelemetry/ProtoTags.cs
+++ src/Boost.Proto.Actor.Opentelemetry/ProtoTags.cs
@@ -4,32 +4,16 @@ namespace Boost.Proto.Actor.Opentelemetry
     {
         public const string ActivitySourceName = "Proto.Actor";
 
-        /// <summary>
-        ///     GetType().Name on the message
-        /// </summary>
         public const string MessageType = "proto.messagetype";
 
-        /// <summary>
-        ///     Message destination
-        /// </summary>
         public const string TargetPID = "proto.targetpid";
 
-        /// <summary>
-        ///     Message origin
-        /// </summary>
         public const string SenderPID = "proto.senderpid";
 
-        /// <summary>
-        ///     Current actor PID, when applicable (equals TargetPID when this is a receive activity, or SenderId when this is a
-        ///     sending activity)
-        /// </summary>
         public const string ActorPID = "proto.actorpid";
 
-        /// <summary>
-        ///     Type of the current actor, when applicable
-        /// </summary>
         public const string ActorType = "proto.actortype";
 
-        public const string ResponseType = "proto.respnsetype";
+        public const string ResponseType = "proto.responsetype";
     }
 }

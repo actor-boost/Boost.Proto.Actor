6725977a4385375953e065e68bb57b205038ea91
SeungYongShim <syshim@me.com>
2022-03-01 11:25:17 +0900

    코드 수정

--- src/Boost.Proto.Actor.Hosting.Cluster.Tests/OptionsSpec.cs
+++ src/Boost.Proto.Actor.Hosting.Cluster.Tests/OptionsSpec.cs
@@ -1,16 +1,31 @@
 using System;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Boost.Proto.Actor.DependencyInjection;
 using FluentAssertions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using Microsoft.Toolkit.HighPerformance;
+using Proto;
+using Proto.Cluster;
 using Xunit;
 using ClusterOptions = Boost.Proto.Actor.Hosting.Cluster.Options;
+using ProtoCluster = global::Proto.Cluster.Cluster;
+using static LanguageExt.Prelude;
 
 namespace Boost.Proto.Actor.Hosting.Cluster.Tests;
+
+public class TestActor : IActor
+{
+    public Task ReceiveAsync(IContext context) => context.Message switch
+    {
+        "Hello" => Task.Run(() => context.Respond("World")),
+        _ => Task.CompletedTask
+    };
+}
 
 public class OptionsSpec
 {
@@ -22,7 +37,7 @@ public class OptionsSpec
     ""Boost"" : {
         ""Actor"" : {
             ""Cluster"" : {
-                ""Name"" : ""UnitTest"",
+                ""Name"" : ""UnitTest1"",
                 ""Provider"" : ""Local""
             }
         }
@@ -35,15 +50,38 @@ public class OptionsSpec
                        {
                            config.AddJsonStream(appsettingsStream);
                        })
-                       .UseProtoActorCluster()
+                       .UseProtoActorCluster((option, sp) =>
+                       {
+                           option.Name = "OverrideClusterName";
+                           option.ClusterKinds.Add(new
+                           (
+                               "TestActor",
+                               sp.GetRequiredService<IPropsFactory<TestActor>>().Create()
+                           ));
+                       })
                        .Build();
 
         await host.StartAsync();
 
         var option = host.Services.GetRequiredService<IOptions<ClusterOptions>>().Value;
 
-        option.Name.Should().Be("UnitTest");
+        option.Name.Should().Be("OverrideClusterName");
         option.Provider.Should().Be(ClusterProviderType.Local);
+        option.ClusterKinds.Should().HaveCount(1);
+        option.ProtoMessages.Should().HaveCount(0);
+        option.AdvertisedHost.Should().Be("127.0.0.1");
+        option.RemoteProvider.Should().Be(RemoteProviderType.GrpcNet);
+
+        var cluster = host.Services.GetRequiredService<ProtoCluster>();
+
+        var cts = new CancellationTokenSource(3 * sec);
+
+        var ret = await cluster.RequestAsync<string>("1",
+                                                     "TestActor",
+                                                     "Hello",
+                                                     cts.Token);
+
+        ret.Should().Be("World");
 
         await host.StopAsync();
     }

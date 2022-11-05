a80367e6a45dcd9f39da12c9e9de7981f1841b70
SeungYongShim <syshim@me.com>
2022-11-05 16:50:53 +0900

    .

---
+++ src/Boost.Proto.Actor.Hosting.Cluster.Fsharp.Tests/Tests.fs
@@ -0,0 +1,50 @@
+module Tests
+
+open System
+open Xunit
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Proto
+open Boost.Proto.Actor.Hosting.Cluster
+open Proto.Cluster
+open Boost.Proto.Actor.DependencyInjection
+
+type TestActorMessage =
+| Hello
+| World
+
+type TestActor() =
+    interface IActor with
+        member _.ReceiveAsync(context) =
+            task {
+                match context.Message with
+                | :? TestActorMessage as msg ->
+                    match msg with
+                    | Hello -> context.Respond(World)
+                    | World -> context.Respond(Hello)
+                | _ -> ()
+            }
+
+let host = Host.CreateDefaultBuilder()
+               .UseProtoActorCluster(Action<_,_>(fun opt -> fun sp ->
+                   opt.Name <- "OverrideClusterName1"
+                   opt.ClusterKinds.Add(ClusterKind
+                       ("TestActor", sp.GetRequiredService<IPropsFactory<TestActor>>().Create())
+                   )))
+               .Build();
+
+host.Start()
+
+[<Fact>]
+let ``My test`` () =
+    let root = host.Services.GetRequiredService<IRootContext>()
+
+    let t = async {
+        let! ct = Async.CancellationToken
+        return! root.System.Cluster().RequestAsync<TestActorMessage>("1", "TestActor", Hello, ct) |> Async.AwaitTask<TestActorMessage>
+    }
+
+    let ret = t |> Async.RunSynchronously
+
+    Assert.Equal(World, ret)
+
